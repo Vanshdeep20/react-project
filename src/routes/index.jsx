dff772d2babce79ec187c2e9a201e225d6b67f13
Vanshdeep20 <155753446+Vanshdeep20@users.noreply.github.com>
2024-02-29 13:14:36 +0530

    demo

---
+++ src/routes/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function index() {
+  return (
+    <div className="container">
+      <h1 className="title">Welcome to my website</h1>{" "}
+      <h3 className="subtitle">Click on home</h3>
+      
+    </div>
+  );
+}
